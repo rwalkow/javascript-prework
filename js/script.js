cb2228cfdd2b3202996a7c99208852f2b3b851d6
Rafal Walkow <r.walkow@po.edu.pl>
2022-03-06 13:33:46 +0100

    Add js code to script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if  (randomNumber == '2'){
+  computerMove = 'papier';
+} else if (randomNumber == '3'){
+  computerMove = 'nożyce';
+}
+
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
@@ -9,7 +23,8 @@ if(playerInput == '1'){
 } else if  (playerInput == '2'){
   playerMove = 'papier';
 } else if (playerInput == '3'){
-  playerMove = 'możyce';
+  playerMove = 'nożyce';
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+printMessage('Mój ruch to: ' + computerMove);
