f41f28e44844d756e129ddcfe6c29de0f8029039
Rafal Walkow <r.walkow@po.edu.pl>
2022-03-09 23:41:17 +0100

    Modify js code in script.js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,28 @@
+'use strict';
+{
 let playerRoundResult=[0,0,0];  // player move: stone, papaer, scissors
 let computerRoundResult=[0,0,0]; // computer move: stone, papaer, scissors
 let gameResult=[0,0,0]; //palyer win, computer win , draw
 let counter = 0;
 const cheatingFactor=10; //cheating factor in % for example 10% - cheat every 10-th move
 
-function playGame(argMoveId){
+const playGame = function(argMoveId){
 
-  function getWinningMove(argMoveId){
+  const getWinningMove = function(argMoveId){
     return (argMoveId %3) +1;
   };
 
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-counter++;
-console.log(counter);
-if (counter >= 100/cheatingFactor) {
-  counter=0;
-  randomNumber = getWinningMove(argMoveId);
-  console.log('Oszukuję'); 
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
+  counter++;
+  console.log(counter);
+  if (counter >= 100/cheatingFactor) {
+   counter=0;
+   randomNumber = getWinningMove(argMoveId);
+   console.log('Oszukuję'); 
 }
 
 //console.log('Wylosowana liczba to: ' + randomNumber);
-
-  function getMoveName(argMoveId){
+  const getMoveName = function(argMoveId){
     const move = ['kamień','papier','nożyce'];
     /*  if (argMoveId < 1 && argMoveId > 3) { //this should never happen, because a random number in the range 1-3 
         return 'nieznany ruch';             // and the buttons only allow selection in the range 1-3
@@ -30,9 +31,8 @@ if (counter >= 100/cheatingFactor) {
   };
 
 
-  function displayResult(argComputerMove, argPlayerMove){
-    let moveTableRows="";
-    let gameTableRows=""; 
+  const displayResult=function(argComputerMove, argPlayerMove){
+    let moveTableRows="", gameTableRows=""; 
     clearMessages();
     printMessage(`Zagrałem ${argComputerMove} , a Ty  ${argPlayerMove}`);
 
@@ -59,16 +59,16 @@ if (counter >= 100/cheatingFactor) {
     gameTableRows=`${gameTableRows}<td>${gameResult[i]}</td>`;
   }
 
-  moveTable = `<table>
+  let moveTable = `<table>
     <tr>
       <th>Ruch</th>
       <th>Gracz</th>
       <th>Komputer</th>
     </tr>
     ${moveTableRows}
-    </table>`;
-  
-  gameTable = `<table>
+    </table>`,  
+    
+    gameTable = `<table>
     <tr>
       <th>Wygrana gracza</th>
       <th>Wygrana komputera</th>
@@ -87,10 +87,11 @@ if (counter >= 100/cheatingFactor) {
 
 playerRoundResult[argMoveId-1]++;
 computerRoundResult[randomNumber-1]++;
-let computerMove = getMoveName(randomNumber);
-let playerInput = argMoveId;
-let playerMove = getMoveName(playerInput);
-displayResult(computerMove, playerMove)
+let computerMove = getMoveName(randomNumber),
+    playerMove = getMoveName(argMoveId);
+
+
+displayResult(computerMove, playerMove);
 }
 
 document.getElementById('play-rock').addEventListener('click', function(){
@@ -102,3 +103,4 @@ document.getElementById('play-paper').addEventListener('click', function(){
 document.getElementById('play-scissors').addEventListener('click', function(){
   playGame(3);
 });
+}
